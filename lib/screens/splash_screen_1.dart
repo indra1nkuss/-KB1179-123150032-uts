2e28a35766cd2c0c0a72fcc9dc65bcde5a889f0e
indra <indra1nkuss@gmail.com>
2025-11-08 00:13:32 +0700

    Menambahkan struktur dasar SplashScreen1 dengan teks sederhana

---
+++ lib/screens/splash_screen_1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Splash Screen 1"),
+      ),
+    );
+  }
+}

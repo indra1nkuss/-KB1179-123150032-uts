e6f77776c41b6c17bca50cdfdb894d57f13b70ae
indra <indra1nkuss@gmail.com>
2025-11-08 01:03:18 +0700

    Menambahkan struktur dasar SplashScreen3 dengan teks sederhana

---
+++ lib/screens/splash_screen_3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen3 extends StatelessWidget {
+  const SplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFFFF8F6),
+      body: Center(
+        child: Text("Splash Screen 3"),
+      ),
+    );
+  }
+}
